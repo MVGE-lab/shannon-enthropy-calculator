6927f36dad75297f2f68307dff0f90a455c5d63c
AFadeew <afadeew@gmail.com>
2014-02-04 16:53:18 +0400

    Script added :)

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 shannon-enthropy-calculator
 ===========================
 
-Python script for calculating of Shannon enthropy value using multiple sequence alignment in fasta format
+Python script for calculating of Shannon enthropy value using multiple sequence alignment in fasta format.
+Produces Tab-delimited list of Shannon enthropy of sequence alignment positions.
+
+USAGE:
+
+python Shannon.py <input_file>
---
+++ Shannon.py
@@ -0,0 +1,70 @@
+import math
+import sys
+#from pylab import *
+r = open(sys.argv[1], 'r')
+w = open(sys.argv[1]+'.output', 'w')
+ids = []
+seqs = []
+n = 0
+for line in r:
+    if line[0] == ">":
+        ids.append(line[1:len(line)-1])
+        n=1
+    else:
+        l = line.strip("\n")
+        if n == 1:
+            seqs.append(l)
+            n = 0
+        else:
+            seqs[len(seqs)-1] = seqs[len(seqs)-1]+l
+Shan = []
+for j in range(0,len(seqs[0])):
+    Acount = 0
+    Gcount = 0
+    Ccount = 0
+    Tcount = 0
+    GAPcount = 0
+    for i in range(0, len(seqs)):
+        if seqs[i][j] == "A":
+            Acount = Acount + 1
+        elif seqs[i][j] == "G":
+            Gcount = Gcount + 1
+        elif seqs[i][j] == "C":
+            Ccount = Ccount + 1
+        elif seqs[i][j] == "T":
+            Tcount = Tcount + 1
+        else:
+            GAPcount = GAPcount + 1
+    PA = float(Acount)/len(seqs)
+    PG = float(Gcount)/len(seqs)
+    PC = float(Ccount)/len(seqs)
+    PT = float(Tcount)/len(seqs)
+    PGAP = float(GAPcount)/len(seqs)
+    if PA != 0.0:
+        AENT = -1*PA*math.log(PA,2)
+    else:
+        AENT = 0.0
+    if PG != 0.0:
+        GENT = -1*PG*math.log(PG,2)
+    else:
+        GENT = 0.0
+    if PC != 0.0:
+        CENT = -1*PC*math.log(PC,2)
+    else:
+        CENT = 0.0
+    if PT != 0.0:
+        TENT = -1*PT*math.log(PT,2)
+    else:
+        TENT = 0.0
+    if PGAP != 0.0:
+        GAPENT = -1*PGAP*math.log(PGAP,2)
+    else:
+        GAPENT = 0.0
+    ShanJ = AENT + GENT + CENT + TENT + GAPENT
+    w.write(str(j)+"\t"+str(ShanJ)+"\n")
+    Shan.append(ShanJ)
+#plot(range(0, len(seqs[0])), Shan, 'r-')
+#savefig(sys.argv[1]+'.png')
+#show()
+r.close
+w.close
